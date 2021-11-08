7efa67ce86dee01796d515312cfb5af484d6b9cc
AudricSkivée <audricskivee@gmail.com>
2021-11-09 00:07:02 +0100

    CRUD for users -> done, CRUD for product WiP

--- routes/product.js
+++ routes/product.js
@@ -31,10 +31,10 @@ router.put('/:id',verifyTokenAndAdmin, async (req,res)=>{
 
 
 //delete
-router.delete("/:id", verifyTokenAndAuthorized, async (req,res)=>{
+router.delete("/:id", verifyTokenAndAdmin, async (req,res)=>{
     try{
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("user has been deleted");
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted");
     }catch(err){
         res.status(500).json('err')
     }
